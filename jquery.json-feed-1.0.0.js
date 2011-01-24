823ce98f66495585836ebaaaaa9d4549fbf01a9d
Dan Drinkard <drinkardd@washpost.com>
2011-01-24 10:10:46 -0500

    more robust callback handling using $.ajax

--- jquery.json-feed-1.0.0.js
+++ jquery.json-feed-1.0.0.js
@@ -1,8 +1,10 @@
 (function($, window, undefined){
     $.fn.JSONFeed = function(opts){
-        
+
         var defaults = {
             url: 'http://projects.washingtonpost.com/moderation/twitter-feed/washington-post-tweets/recent.json',
+            jsonp: 'callback',
+            jsonpCallback: '?',
             iterator: function(data){ return data; },
             num: 5,
             spinner: '<span>Loading...</span>',
@@ -18,21 +20,39 @@
                 .replace(options.optionP, function($0,$1){return options[$1] || ''});
             return options.renderCallback(str);
         };
-        
+
         return $(this).each(function(){
             var self = this;
-            $(self).append($(options.spinner).addClass('jf-spinner'));
-            $.getJSON(options.url, 'callback=?', function(data, status){
-                $(self).find('.jf-spinner').remove();
-                $.each(options.iterator(data), function(idx, item){
-                    if(idx<options.num){
-                        $(self).append(render(item));
+            var ajaxOpts = {
+                url: options.url,
+                dataType: (options.jsonpCallback) ? 'jsonp': 'json',
+                success: function(data, status){
+                    $(self).find('.jf-spinner').remove();
+                    $.each(options.iterator(data), function(idx, item){
+                        if(idx<options.num){
+                            $(self).append(render(item));
+                        }else{
+                            return false;
+                        }
+                    });
+                    $(self).each(options.appendCallback);
+                }
+            }
+            if(options.jsonpCallback){
+                if(options.jsonpCallback != '?'){
+                    ajaxOpts.jsonpCallback = options.jsonpCallback;
+                    ajaxOpts.jsonp = options.jsonp;
+                }else{
+                    var frags = ajaxOpts.url.split('?');
+                    if(frags.length > 1){
+                        ajaxOpts.url += '&' + options.jsonp + '=' + options.jsonpCallback;
                     }else{
-                        return false;
+                        ajaxOpts.url += '?' + options.jsonp + '=' + options.jsonpCallback;
                     }
-                });
-                $(self).each(options.appendCallback);
-            });
+                }
+            }
+            $(self).append($(options.spinner).addClass('jf-spinner'));
+            $.ajax(ajaxOpts);
         });
     }
     $.fn.JSONFeed.docsURL = 'http://github.com/dandrinkard/jquery-json-feed/';
