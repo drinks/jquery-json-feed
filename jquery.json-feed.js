119a9dd29fd352dafc3ebe99961ffbae4dcb2091
Dan Drinkard <drinkardd@washpost.com>
2011-04-22 14:16:57 -0400

    pagination & polling support, twitter api/pagination/oembed preset

--- jquery.json-feed.js
+++ jquery.json-feed.js
@@ -1,3 +1,58 @@
+/**
+ * jquery-json-feed
+ * ================
+ * JSONP -> html with recursive parsing
+ *
+ * Usage:
+ * ------
+ * -- *HTML* --
+ * <div class="my-feed" data-url="myfeed.json"></div>
+ *
+ * -- *JavaScript* --
+ * $('.myfeed).JSONFeed();
+ *
+ * Class attributes (Set via function options {}):
+ * --------------------------------------------
+ * @string url: The url to fetch, globally settable
+ * @string jsonp: The name of the callback parameter
+ * @string jsonpCallback: The value of the callback parameter
+ * @function iterator: Callback function that returns a list of data objects to render
+ * @int num: Number of results to render per page
+ * @int poll: Number of seconds between content refreshes; 0 disables
+ * @function pollUrlGenerator: Callback function that returns the next url for the poller
+ *                             (handy for setting since_id, etc)
+ * @bool paginate: Whether or not to show a 'Show more' button
+ * @function paginationUrlGenerator: Callback function that returns the next url for the paginator
+ * @string spinner: HTML markup for a spinner element to display while results are loading
+ * @string template: HTML markup for an individual result item
+ * @regexp jsonVarP: Pattern to match/capture variable references in the template
+ * @regexp optionP: Pattern to match/capture option references in the template
+ * @function renderCallback: Function that returns a modified rendered template
+ * @function appendCallback: Function called after a page of items are appended to the container
+ *
+ * Instance attributes (Set via data-<attr>):
+ * -----------------------------------------
+ * @string url: Overrides the global url
+ * @int num: Overrides the number of results to show
+ * @int poll: Overrides the global poll interval
+ * @bool paginate: Overrides the global paginate var
+ *
+ * Instance private attrs (data('_key')):
+ * --------------------------------------
+ * @object _poll:
+ *   lastUrl
+ *   lastTime
+ *   interval
+ *   count
+ *   page
+ *   data
+ *
+ * @object _page:
+ *   lastUrl
+ *   page
+ *   data
+ *
+ */
 (function($, window, undefined){
     $.fn.JSONFeed = function(opts){
 
@@ -7,7 +62,17 @@
             jsonpCallback: '?',
             iterator: function(data){ return data; },
             num: 5,
+            poll: 0,
+            pollUrlGenerator: function(prev_url, prev_response, options){
+              return prev_url;
+            },
+            paginate: false,
+            paginationUrlGenerator: function(prev_url, prev_response, options){
+              return prev_url;
+            },
             spinner: '<span>Loading...</span>',
+            newTweetsMessage: '<a href="#">Click to show new tweets.</a>',
+            paginationButton: '<a href="#">Load more...</a>',
             template: '<p style="color:#a00">You haven&apos;t set a template for this feed!</p>',
             jsonVarP: /\{\{([^\}]+)\}\}/gm,
             optionP: /\{%([^%]+)%\}/gm,
@@ -15,6 +80,84 @@
             appendCallback: function(){}
         },
         options = $.extend({}, defaults, opts),
+        is_unique = function(el){
+            if($(el).attr('id')){
+                console.log($(el).attr('id'));
+                console.log($('#'+$(el).attr('id')));
+                return !$('#'+$(el).attr('id')).length;
+            }else{
+                // always unique if no ids
+                return true
+            }
+        }
+        request = function(url, ctx){
+            var self = $(ctx),
+                action = (self.attr('className').match(/page-/)!== null ? 'poll': 'page'),
+                num = self.parent().attr('data-num') || options.num,
+                ajaxOpts = {
+                    url: url || options.url,
+                    dataType: (options.jsonpCallback) ? 'jsonp': 'json',
+                    success: function(data, status){
+                        self.find('.jf-spinner').remove();
+                        var objs = options.iterator(data);
+                        console.log(objs);
+                        $.each(objs, function(idx, item){
+                            // exempt from limit if this is a polling request
+                            if(idx<num || action=='poll'){
+                                var el = $(render(item));
+                                if(is_unique(el)){
+                                    console.log('appending...');
+                                    self.append(el);
+                                }
+                            }else{
+                                console.log('not apending: out of range...');
+                                return false;
+                            }
+                        });
+                        // handle buttons & data cache
+                        if(action=='poll'){
+                            if(self.children().length && self.is(':hidden')){
+                                var showBtn = self.siblings('.jf-show-new');
+                                if(showBtn.length){
+                                    self.before(showBtn);
+                                }else{
+                                    self.before(options.newTweetsMessage).prev().addClass('jf-show-new');
+                                }
+                            }
+                            var _poll = self.parent().data('_poll');
+                            _poll['data'] = data;
+                            self.parent().data('_poll', _poll);
+                        }else{
+                            if(objs.length == 0){ // last page
+                                self.siblings('.jf-show-more').remove();
+                            }else{
+                                self.siblings('.jf-show-more').removeClass('disabled');
+                                var _page = self.parent().data('_page');
+                                _page['data'] = data;
+                                self.parent().data('_page', _page);
+                            }
+                        }
+
+                        self.each(options.appendCallback);
+                    }
+                }
+
+            if(options.jsonpCallback){
+                if(options.jsonpCallback != '?'){
+                    ajaxOpts.jsonpCallback = options.jsonpCallback;
+                    ajaxOpts.jsonp = options.jsonp;
+                }else{
+                    var frags = ajaxOpts.url.split('?');
+                    ajaxOpts.url += ((frags.length > 1) ? '&' : '?') + options.jsonp + '=' + options.jsonpCallback;
+                }
+            }
+            $(self).append($(options.spinner).addClass('jf-spinner'));
+            if(ajaxOpts.url){
+                $.ajax(ajaxOpts);
+            }else{
+                ajaxOpts.success([], 'skipped');
+            }
+        },
         render = function(data){
             // data
             var str = options.template.replace(options.jsonVarP, function($0,$1){
@@ -39,45 +182,75 @@
                     return response;
                 });
             return options.renderCallback(str);
+        },
+        pageFactory = function(num){
+            return '<div class="jf-page page'+num+'"></div>';
+        }
+        poll = function(ctx){
+            var self = $(ctx),
+                locals = self.data('_poll');
+            locals.count += 1;
+            var page = self.find('.jf-page').eq(0);
+            // if the last page isn't empty, create a new one
+            // otherwise reuse it
+            if(page.children().length > 0){
+                locals.page += 1;
+                page = self.prepend(pageFactory(locals.page*-1)).find('.page-'+locals.page).eq(0).hide();
+            }
+            locals.lastUrl = options.pollUrlGenerator(locals.lastUrl, locals['data'], options);
+            self.data('_poll', locals);
+            request(locals.lastUrl, page);
+            window.setTimeout(function(){poll(self)}, locals.interval*1000);
+        },
+        paginate = function(ctx){
+            var self = $(ctx),
+                locals = self.data('_page');
+            locals.page += 1;
+            console.log(locals.lastUrl);
+            var page = self.find('.jf-page').eq(-1).after(pageFactory(locals.page)).next();
+            locals.lastUrl = options.paginationUrlGenerator(locals.lastUrl, locals['data'], options);
+            self.data('_page', locals);
+            request(locals.lastUrl, page);
         };
 
         return $(this).each(function(){
-            var self = this;
-            var ajaxOpts = {
-                url: $(self).attr('data-url') || options.url,
-                dataType: (options.jsonpCallback) ? 'jsonp': 'json',
-                success: function(data, status){
-                    $(self).find('.jf-spinner').remove();
-                    $.each(options.iterator(data), function(idx, item){
-                        if(idx<options.num){
-                            $(self).append(render(item));
-                        }else{
-                            return false;
-                        }
-                    });
-                    $(self).each(options.appendCallback);
-                }
+            var self = $(this),
+                locals = {
+                    url: self.attr('data-url') || options.url,
+                    poll: self.attr('data-poll') || options.poll,
+                    paginate: self.attr('data-paginate') || options.paginate
+                },
+                page = self.append(pageFactory(0)).find('.page0').eq(0);
+            request(locals.url, page);
+            $(this).delegate('a.jf-show-new', 'click', function(e){
+                e.preventDefault();
+                e.stopPropagation();
+                $(this).nextAll('.jf-page').filter(':hidden').show()
+                $(this).remove();
+            });
+            if(locals.poll > 0){
+                self.data('_poll', {
+                    lastUrl: locals.url,
+                    lastTime: new Date(),
+                    interval: locals.poll,
+                    count: 0,
+                    page:0
+                });
+                window.setTimeout(function(){poll(self)}, locals.poll*1000);
             }
-            if(options.jsonpCallback){
-                if(options.jsonpCallback != '?'){
-                    ajaxOpts.jsonpCallback = options.jsonpCallback;
-                    ajaxOpts.jsonp = options.jsonp;
-                }else{
-                    var frags = ajaxOpts.url.split('?');
-                    if(frags.length > 1){
-                        ajaxOpts.url += '&' + options.jsonp + '=' + options.jsonpCallback;
-                    }else{
-                        ajaxOpts.url += '?' + options.jsonp + '=' + options.jsonpCallback;
+            if(locals.paginate == 'true'){
+                self.data('_page', {
+                    lastUrl: locals.url,
+                    page: 0,
+                });
+                self.append($(options.paginationButton).addClass('jf-show-more').click(function(e){
+                    e.preventDefault();
+                    if(!$(this).hasClass('.disabled')){
+                        $(this).addClass('disabled');
+                        paginate(self);
                     }
-                }
+                }));
             }
-            $(self).append($(options.spinner).addClass('jf-spinner'));
-            if(ajaxOpts.url){
-                $.ajax(ajaxOpts);
-            }else{
-                ajaxOpts.success([], 'skipped');
-            }
-
         });
     }
     $.fn.JSONFeed.docsURL = 'http://github.com/dandrinkard/jquery-json-feed/';
--- presets/jf.preset.twitter-paginated-oembed.js
+++ presets/jf.preset.twitter-paginated-oembed.js
@@ -2,33 +2,67 @@
     $.extend($.fn.JSONFeed.presets, {
         'twitterPaginatedOembed': {
             /**
-             * Twitter User Feed, oEmbed via embedly
-             * @requires twttr.anywhere <http://dev.twitter.com/anywhere>
-             * @requires embedly-jquery <https://github.com/embedly/embedly-jquery>
-             * @requires utils.activateLinks
-             * @requires utils.hoverCardsWithUsernames
-             */
-             template: '<div class="jf-tweet">\
-                             <a href="http://twitter.com/{{user.screen_name}}"><img src="{{user.profile_image_url}}" alt="{{user.screen_name}}" width="{%avatar_width%}" height="{%avatar_height%}" /></a>\
-                             <p class="jf-meta">\
-                                 <a class="anywhere-username" href="http://twitter.com/{{user.screen_name}}">{{user.screen_name}}</a> - \
-                                 <a class="jf-timestamp" href="http://twitter.com/{{user.screen_name}}/status/{{id_str}}">{{created_at}}</a></p>\
-                             <p class="jf-tweet">{{text}}</p>\
-                         </div>',
-             iterator: function(data){ return data; },
-             renderCallback: function(str){
-                 var str = $(this).JSONFeed.utils.activateLinks(str);
-                 return $(this).JSONFeed.utils.activateTwitterHashTags(str);
-                 return str;
-             },
-             appendCallback: function(){
-                 $(this).find('.jf-timestamp').each(function() {
-                     var date = new Date(Date.parse($(this).text()));
-                     $(this).text(date.toUTCString()).relativeDate();
-                 });
-                 $(this).JSONFeed.utils.hovercardsWithUsernames(this);
-                 $(this).find('p.jf-tweet a').embedly({urlRe:/(https?:\/\/.*)/i});
-             }
+            * Twitter User Feed, oEmbed via embedly
+            * @requires twttr.anywhere <http://dev.twitter.com/anywhere>
+            * @requires embedly-jquery <https://github.com/embedly/embedly-jquery>
+            * @requires utils.activateLinks
+            * @requires utils.hoverCardsWithUsernames
+            */
+            template: '<div class="jf-tweet" id="tweet-{{id_str}}">\
+            <a href="http://twitter.com/{{user.screen_name}}"><img src="{{user.profile_image_url}}" alt="{{user.screen_name}}" width="{%avatar_width%}" height="{%avatar_height%}" /></a>\
+            <p class="jf-meta">\
+            <a class="anywhere-username" href="http://twitter.com/{{user.screen_name}}">{{user.screen_name}}</a> - \
+            <a class="jf-timestamp" href="http://twitter.com/{{user.screen_name}}/status/{{id_str}}" title="{{created_at}}"></a></p>\
+            <p class="jf-tweet">{{text}}</p>\
+            </div>',
+            iterator: function(data){ return data; },
+            pollUrlGenerator: function(url, data, options){
+                var objs = options.iterator(data);
+                if(objs && objs.length){
+                    var since_id = objs[0].id_str;
+                    if(url.match('since_id') !== null){
+                        url = url.replace(/since_id=[0-9]+/, 'since_id=' + since_id);
+                    }else{
+                        if(url.match(/\?/) !== null){
+                            url += '&since_id=' + since_id;
+                        }else{
+                            url += '?since_id=' + since_id;
+                        }
+                    }
+                }
+                window.console && console.log && console.log('polling:', url);
+                return url;
+            },
+            paginationUrlGenerator: function(url, data, options){
+                var objs = options.iterator(data);
+                if(objs && objs.length){
+                    var max_id = objs[objs.length-1].id_str;
+                    if(url.match('max_id') !== null){
+                        url = url.replace(/max_id=[0-9]+/, 'max_id=' + max_id);
+                    }else{
+                        if(url.match(/\?/) !== null){
+                            url += '&max_id=' + max_id;
+                        }else{
+                            url += '?max_id=' + max_id;
+                        }
+                    }
+                }
+                window.console && console.log && console.log('paginating:', url);
+                return url;
+            },
+            renderCallback: function(str){
+                var str = $(this).JSONFeed.utils.activateLinks(str);
+                return $(this).JSONFeed.utils.activateTwitterHashTags(str);
+                return str;
+            },
+            appendCallback: function(){
+                $(this).parent().find('.jf-timestamp').each(function() {
+                    var date = new Date(Date.parse($(this).attr('title')));
+                    $(this).text(date.toUTCString()).relativeDate();
+                });
+                $(this).JSONFeed.utils.hovercardsWithUsernames(this);
+                $(this).find('p.jf-tweet a').embedly();
+            }
         }
     });
 })(jQuery);
